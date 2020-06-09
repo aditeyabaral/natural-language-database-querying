a0b7129e2fe3c951cd9e2153c603ee685013b939
Aditeya Baral <33230959+aditeyabaral@users.noreply.github.com>
2020-06-10 00:23:05 +0530

    Add files via upload

---
+++ speechRecognition.py
@@ -0,0 +1,46 @@
+import speech_recognition as sr
+from googletrans import Translator
+from moviepy.editor import VideoFileClip
+
+def audio_to_text(epoch = 1):
+    r = sr.Recognizer()
+    m = sr.Microphone()
+    value, translated = None, None
+    with m as source: r.adjust_for_ambient_noise(source)
+    print("Recording...")
+    with m as source: audio = r.listen(source)
+    while epoch>0:
+        try:
+            value = r.recognize_google(audio, show_all = False) #fr-FR, hi-IN,kn-IN,ta-IN
+            translated = translate(value)
+        except sr.UnknownValueError:
+            print("Didn't catch that!")
+        epoch-= 1
+    return value, translated
+
+def video_to_text(filename):
+    video = VideoFileClip(filename)
+    audio_obj = video.audio
+    short_filename = filename[max(0,filename.rfind("/"))+1:filename.rfind(".")]
+    audio_obj.write_audiofile(r"Database/Audio/{}.wav".format(short_filename))
+    r = sr.Recognizer()
+    audiofile = sr.AudioFile(r"Database/Audio/{}.wav".format(short_filename))
+    with audiofile as source:
+        audio = r.record(source)
+    transcript = r.recognize_google(audio, show_all = False)
+    translated = translate(transcript)
+    return transcript, translated
+
+def translate(query):
+    translator = Translator()
+    source_details = translator.detect(query)
+    print(source_details)
+    if source_details.lang != "en":
+        query = translator.translate(query, src = source_details.lang, dest = "en")
+        return query.text
+    else:
+        return query
+
+
+#value, translated = audio_to_text()
+#transcript, translated = video_to_text(r"Database/Video/sample2.mp4")
