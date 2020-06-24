ebeed0671f68136071596cc8bdd7322a485d23ab
Anirudh H M <anihm136@gmail.com>
2020-06-24 19:42:51 +0530

    Add exact keyword match

--- Video Tagging/queryRetrieval.py
+++ Video Tagging/queryRetrieval.py
@@ -1,28 +1,41 @@
-import speechRecognition
-import keywords
-import time
 import csv
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+import time
+
+from nltk.stem import WordNetLemmatizer
+
+import keywords
+import speechRecognition
+
+lemm = WordNetLemmatizer()
 
 
 query, translated = speechRecognition.audio_to_text()
 while not translated:
-  time.sleep(2)
-  query, translated = speechRecognition.audio_to_text()
+    time.sleep(2)
+    query, translated = speechRecognition.audio_to_text()
 
-tags = keywords.getKeywordsRAKE(translated)
+print(query)
+print(translated)
+
+tags = list(map(lemm.lemmatize, keywords.getKeywordsRAKE(translated)))
+print(tags)
 vid_name, vid_tags = [], []
 with open("videoTags.csv", "r") as tagsfile:
-  reader = csv.reader(tagsfile)
-  for row in reader:
-    vid_tags.append(" ".join(row[1:]))
-    vid_name.append(row[0])
+    reader = csv.reader(tagsfile)
+    for row in reader:
+        vid_tags.append(list(map(lemm.lemmatize, row[1:])))
+        vid_name.append(row[0])
 
-transform_mat = vid_tags + [" ".join(tags)]
+sim_scores = []
+tags_set = set(tags)
+for vid_tag_list in vid_tags:
+    vid_tag_set = set(vid_tag_list)
+    sim_scores.append(len(tags_set.intersection(vid_tag_set)))
 
-vectorizer = CountVectorizer()
-model = vectorizer.fit_transform(transform_mat)
-for vid in model[:-1]:
-  print(cosine_similarity(model[-1], vid))
+position = sim_scores.index(max(sim_scores))
+print("Score: ", sim_scores[position])
+most_similar_video = vid_name[position]
+print(most_similar_video)
 
+
+# transform_mat = vid_tags + [" ".join(tags)]
